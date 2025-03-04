ea0fa28b453ac1c47728e4512f5bad31a676c931
TIN TRINH <tin.ttrinh0@gmail.com>
2025-03-04 09:15:27 -0800

    Add database

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,11 @@
 //---------------------------------
 
 
-function sayHello() {
-    //do something
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+    }).catch((error) => {
+        // An error happened.
+    });
 }
-//sayHello();    //invoke function
